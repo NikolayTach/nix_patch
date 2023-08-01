6d291409575b1cbb4562ad9d4795e0ef57b6af29
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2023-08-01 11:27:45 -0700

    Add files via upload

---
+++ 05cwymiy8jbp173qh99q8w22xrgq0vav-bobo1.c
@@ -0,0 +1,14 @@
+#include "bobo.h"
+
+int main() {
+    // Assuming you want to execute a bash command to transition from "java-nogue.io" to "java.nogue.io/main"
+    system("bash -c \"java-nogue.io to java.nogue.io/main\"");
+    
+    // Assuming you want to set a Specification Unit to "Flushable"
+    SpecificationUnit unit;
+    unit.property = "Flushable";
+    
+    // Rest of your code...
+    
+    return 0;
+}

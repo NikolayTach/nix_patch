03f61a8a0547eda7601111a2d06c26fd073cac4f
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2024-10-05 21:49:58 -0700

    Update mouseClick.java

--- mouseClick.java
+++ mouseClick.java
@@ -4,7 +4,12 @@ Button button = new Button("Click me!");
 button.addMouseListener(new MouseAdapter() {
     @Override
     public void mouseClicked(MouseEvent e) {
-        MouseEvent clickedEvent = createClicked(button, 10, 20);
+        CustomMouseEvent clickedEvent = new CustomMouseEvent(button, 
+System.currentTimeMillis(), 10, 20, MouseEvent.MOUSE_CLICKED, false);
         // Process the clicked event here...
     }
 });
+
+// Another way to create a custom mouse event
+CustomMouseEvent anotherClickedEvent = new CustomMouseEvent(button, 
+System.currentTimeMillis(), 30, 40, MouseEvent.MOUSE_CLICKED, false, 15, 25);
